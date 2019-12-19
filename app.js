f4b1d2f7b1be6318e3c417e09ca7fd0ccbc80afd
Jessica Gibbins <jessagibbins@gmail.com>
2019-12-19 14:29:16 -0500

    Fix bugs

--- app.js
+++ app.js
@@ -1,19 +1,19 @@
 /* ---------- Modal Functionality ---------- */
 
 $(document).ready(function(){
-    $(".myBtn1").click(function(){
-        $(".myModal1").modal();
+    $("#myBtn1").click(function(){
+        $("#myModal1").modal();
     });
 
-    $(".myBtn2").click(function(){
-        $(".myModal2").modal();
+    $("#myBtn2").click(function(){
+        $("#myModal2").modal();
     });
 
-    $(".myBtn3").click(function(){
-        $(".myModal3").modal();
+    $("#myBtn3").click(function(){
+        $("#myModal3").modal();
     });
 
-    $(".myBtn4").click(function(){
-        $(".myModal4").modal();
+    $("#myBtn4").click(function(){
+        $("#myModal4").modal();
     });
 });
