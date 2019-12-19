4f25a3e37a2e98893f37032fc5f9e8d2a09a6f6b
Jessica Gibbins <jessagibbins@gmail.com>
2019-12-19 12:22:42 -0500

    Add modal functionality

---
+++ app.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+    $(".myBtn1").click(function(){
+        $(".myModal1").modal();
+    });
+
+    $(".myBtn2").click(function(){
+        $(".myModal2").modal();
+    });
+
+    $(".myBtn3").click(function(){
+        $(".myModal3").modal();
+    });
+
+    $(".myBtn4").click(function(){
+        $(".myModal4").modal();
+    });
+});
